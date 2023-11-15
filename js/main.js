f091231d209a5ccfcd8148f487c6e5b32227086a
Maksim-Salnikov <salomaksim123@gmail.com>
2023-11-15 16:11:47 +0300

    create: adaptation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 var swiper = new Swiper('.swiper', {
-  slidesPerView: 2,
+  slidesPerView: 1,
   loop: true,
   navigation: {
     nextEl: '.main-series-button',
   },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+  },
 });
+
+var headerButton = document.querySelector(".header-button");
+var headerWindow = document.querySelector(".header");
+var middleLine = document.querySelector(".header-button__line");
+
+function toggleModal(){
+  headerWindow.classList.toggle("header--visible");
+  headerButton.classList.toggle("header-button--active");
+  middleLine.classList.toggle("header-button__line--active");
+}
+
+headerButton.addEventListener("click", toggleModal);
+
